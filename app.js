8d78da9ac45c6e686d71e5bafba6253db2b15ec9
grayni <omenbestg@gmail.com>
2021-03-09 00:19:13 +0300

    behavior inputs and styles

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-import './upload.js'
+import {upload} from './upload.js'
 
 console.log('app.js work!!')
+
+upload('#pictures', {
+  multi: true,
+  accept: ['.png', '.jpeg', '.jpg', '.gif']
+})
--- upload.js
+++ upload.js
@@ -1,1 +1,60 @@
-console.log('upload.js work!!!')
+export const upload = (selector, options = {}) => {
+  const inputPictures = document.querySelector(selector)
+
+  const openButton = document.createElement('button')
+  openButton.classList.add('btn')
+  openButton.textContent = 'Открыть'
+
+  const preview = document.createElement('div')
+  preview.classList.add('preview')
+
+
+  if (options.multi) {
+    inputPictures.setAttribute('multiple', true)
+  }
+
+  inputPictures.insertAdjacentElement('afterend', preview)
+  inputPictures.insertAdjacentElement('afterend', openButton)
+
+  openButton.addEventListener('click', () => {
+    inputPictures.click()
+  })
+
+  if (options.accept && Array.isArray(options.accept)) {
+    inputPictures.setAttribute('accept', options.accept.join(','))
+  }
+
+  const changeHandler = event => {
+    if (!event.target.files.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files, file => {
+      preview.innerHTML = ''
+
+      const reader = new FileReader()
+
+      if (!file.type.match('image')) {
+        return
+      }
+
+      reader.onload = ev => {
+        const src = ev.target.result
+        preview.insertAdjacentHTML('afterbegin', `
+        <div class="preview-image">
+          <div class="preview-remove">&times;</div>
+          <img src=${src} alt=${file.name}>
+          <div class="preview-info">
+            <p>${file.name}</p>
+            <p>${(file.size/(1024*1024)).toFixed(2)} Mb</p>
+          </div>
+        </div>`)
+      }
+
+      reader.readAsDataURL(file)
+      console.log(file)
+    })
+  }
+
+  inputPictures.addEventListener('change', changeHandler)
+}
